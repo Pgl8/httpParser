a4795dadb6cc3f4450fff90525b025c36df40ce4
Pgl8 <pepe.gl@gmail.com>
2014-08-19 16:10:24 +0200

    Cambios xml

--- app/src/main/java/com/pgl8/httpparser/Principal.java
+++ app/src/main/java/com/pgl8/httpparser/Principal.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
